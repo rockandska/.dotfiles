06313dbf768aa61d754e497db677fa33ecbe1fb4
RockAndSka <yoann_mac_donald@hotmail.com>
2018-06-28 23:11:04 +0200

    Download config for perl instead contruct it ~on the fly Create a volatile directory for tmp myrepos Ask to the user to move his ~/.mrconfig if it already exists Display final message with success Rename perl.sh to 00perl.sh to load it first later with *

--- .config/myrepos/bootstrap.sh
+++ .config/myrepos/bootstrap.sh
@@ -2,50 +2,83 @@
 set -eo pipefail
 
 # Which version of perl is installed ?
-PERL_MAJOR_VERSION=$(perl -e 'print $];' \
-			| sed 's/\..*//' 2> /dev/null \
-			|| >&2 echo "Error while getting Perl version")
+PERL_MAJOR_VERSION=$(	perl -e 'print $];' \
+			| sed 's/\..*//' 2> /dev/null ) \
+				|| { >&2 echo "Error while getting Perl version"; exit 1; }
 
 # Init  with default values or args
 BOOTSTRAP_MRCONFIG_URL="${1:-https://raw.githubusercontent.com/rockandska/.dotfiles/master/.mrconfig}"
-PERL_LIBPATH="${2:-${HOME:?}/.local/opt/perl${PERL_MAJOR_VERSION}}"
+BOOTSTRAP_PERL_LIBPATH_URL="${2:-https://raw.githubusercontent.com/rockandska/.dotfiles/master/.config/bash/conf.d/00perl.sh}"
 
 MYREPOS_REPO='git://myrepos.branchable.com/'
 MYREPOS_BRANCH='master'
 
+if [ -e ~/.mrconfig ] || [ -h ~/.mrconfig ];then
+	>&2 echo "#########################################################################################"
+	>&2 echo "#                                                                                       #"
+	>&2 echo "# ~/.mrconfig already exist on your system. Please remove it before running this script #"
+	>&2 echo "#                                                                                       #"
+	>&2 echo "#########################################################################################"
+	exit 1
+fi
+
+
+######
+# Prepare tmp directory for myrepos
+######
+TMP_MYREPOS_DIR=$(mktemp -d --suffix=_myrepos)
+if [[ ! -d "${TMP_MYREPOS_DIR:?}" ]]; then
+	echo "Could not create temp dir"
+	exit 1
+fi
+function cleanup {      
+	rm -rf "${TMP_MYREPOS_DIR:?}"
+	echo "Deleting temp working directory ${TMP_MYREPOS_DIR:?}"
+}
+trap cleanup EXIT ERR
+
 cd ${HOME:?\$HOME not defined}
 
-# Configure perl locallib variables
-read -r -d '' PERL_CONFIG <<- EOF || true
-	PERL${PERL_MAJOR_VERSION}LIB="${PERL_LIBPATH:?}/lib/perl${PERL_MAJOR_VERSION}${PERL5LIB:+:${PERL5LIB}}"; export PERL${PERL_MAJOR_VERSION}LIB
-	PERL_LOCAL_LIB_ROOT="${PERL_LIBPATH:?}${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT
-	PERL_MB_OPT="--install_base \"${PERL_LIBPATH:?}\""; export PERL_MB_OPT
-	PERL_MM_OPT="INSTALL_BASE=${PERL_LIBPATH:?}"; export PERL_MM_OPT
-	PATH="${PERL_LIBPATH:?}/bin:${PATH:?}"; export PATH
-EOF
-
-eval ${PERL_CONFIG}
+######
+# Init Perl config
+######
+wget -q -O - ${BOOTSTRAP_PERL_LIBPATH_URL} | bash
 
 ######
 # MYREPOS TMP INSTALL
 ######
-rm -rf /tmp/myrepos || echo >&2 "Can't delete /tmp/myrepos"
 echo "Cloning MyRepos from ${MYREPOS_REPO}"
-git clone -q -b ${MYREPOS_BRANCH} ${MYREPOS_REPO} /tmp/myrepos
+git clone -q -b ${MYREPOS_BRANCH} ${MYREPOS_REPO} ${TMP_MYREPOS_DIR}
 
 ######
 # BOOTSTRAP FROM URL
 ######
-rm -rf ~/.mrconfig || echo >&2 "Can't delete ~/.mrconfig"
-echo "Bootstrap from ${BOOTSTRAP_MRCONFIG_URL:?} (could take some minutes...)"
-wget -q -O - ${BOOTSTRAP_MRCONFIG_URL:?} | /tmp/myrepos/mr --quiet --force --trust-all -d ~/ bootstrap - ~/
+echo "Bootstraping from ${BOOTSTRAP_MRCONFIG_URL:?} (could take some minutes...)"
+wget -q -O - ${BOOTSTRAP_MRCONFIG_URL:?} | ${TMP_MYREPOS_DIR}/mr --quiet --force --trust-all -d ~/ bootstrap - ~/
 
 #####
-# BACKUP FILES ALREADY PRESENT
+# BACKUP FILES ALREADY PRESENT AND STOW NEW ONES
 #####
 mr -m misstowed --backup-and-stow
+
+#####
+# RESTART BASH
+#####
+
+exec bash
 
 #####
 # CHECKOUT AGAIN NOW WE HAVE OUR DEFAULT CONFIG FILES
 #####
 mr -m checkout
+
+
+
+#####
+# EXIT
+#####
+echo -e "###############################################################"
+echo -e "#                                                             #"
+echo -e "# Please restart bash to take into account the new parameters #"
+echo -e "#                                                             #"
+echo -e "###############################################################"
