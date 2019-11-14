72a56b2126afbd4769df05304813d62a206d66ea
rockandska <yoann_mac_donald@hotmail.com>
2019-11-14 16:26:33 +0100

    [base16-shell] simpler loading of color theme

--- .config/bash/conf.d/bash16-shell.sh
+++ .config/bash/conf.d/bash16-shell.sh
@@ -1,7 +1,1 @@
-# Base16 Shell
-BASE16_SHELL="$HOME/.local/opt/base16-shell/"
-[ -n "$PS1" ] && \
-	[ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-		eval "$("$BASE16_SHELL/profile_helper.sh")"
-
-base16_solarized-dark
+source /home/rockandska/.local/opt/base16-shell/scripts/base16-solarized-dark.sh solarized-dark
