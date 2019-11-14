0d9049f2eb09901b21146ac1d38babf28404853b
rockandska <yoann_mac_donald@hotmail.com>
2019-11-14 21:16:13 +0100

    Move dotfiles to ~/.local/opt/dotfiles

--- .config/bash/aliases.d/dotfiles.sh
+++ .config/bash/aliases.d/dotfiles.sh
@@ -1,4 +1,4 @@
-alias dotfiles="/usr/bin/env git --git-dir=\$HOME/.local/src/dotfiles/.git --work-tree=\$HOME"
+alias dotfiles="/usr/bin/env git --git-dir=\$HOME/.local/opt/dotfiles/.git --work-tree=\$HOME"
 # Load git completion to use the same one for dotfiles
 eval "$(complete | grep -- ' -D' | grep -o -- '-F [a-zA-Z_-]* ' | sed 's/-F //;s/ $//') git"
 eval "$(complete | grep ' git$' | sed 's/ git$/ dotfiles'/)"
