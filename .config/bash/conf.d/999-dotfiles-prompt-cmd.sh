a1ddf833a9946b7720496749bbeaa0061fe9456d
rockandska <yoann_mac_donald@hotmail.com>
2019-11-15 22:35:35 +0100

    [dotfiles] add a directory in ~/local for bootstrap files etc..

--- .config/bash/conf.d/999-dotfiles-prompt-cmd.sh
+++ .config/bash/conf.d/999-dotfiles-prompt-cmd.sh
@@ -1,11 +1,14 @@
 _check_dotfiles_gitdir() {
-	local dotfiles_gitdir="${HOME}/.local/opt/dotfiles/.git"
-	if [[ "${PWD}" == "${HOME}" ]];then
+	local dotfiles_gitdir="${HOME}/.local/opt/dotfiles"
+	local dotfiles_bootstrap="${HOME}/.local/opt/dotfiles-bootstrap"
+	if [[ "${PWD}" == "${HOME}" ]] \
+		|| [[ "${PWD}" == "${dotfiles_gitdir}" ]] || [[ "${PWD}" ==	"${dotfiles_gitdir}"/* ]] \
+		|| [[ "${PWD}" == "${dotfiles_bootstrap}" ]] || [[ "${PWD}" == "${dotfiles_bootstrap}"/* ]];then
 		export GIT_DIR="${dotfiles_gitdir}"
-		export GIT_WORKTREE="${HOME}"
+		export GIT_WORK_TREE="${HOME}"
 	else
-		[[ "${GIT_DIR}" == "${dotfiles_gitdir}" ]] && unset GIT_DIR
-		[[ "${GIT_WORKTREE}" == "${HOME}" ]] && unset GIT_WORKTREE
+		[[ "${GIT_DIR}" == "${dotfiles_gitdir}" ]] && unset GIT_DIR || true
+		[[ "${GIT_WORK_TREE}" == "${HOME}" ]] && unset GIT_WORK_TREE || true
 	fi
 }
 
---
+++ .local/opt/dotfiles-bootstrap/README.md
@@ -0,0 +1,1 @@
+/home/rockandska/README.md
